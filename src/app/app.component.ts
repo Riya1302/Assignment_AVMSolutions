e28983a282299a55c8718ab2c468d8aa388465c2
Riya <gautamriya646@gmail.com>
2023-01-05 15:57:29 +0530

    feat: updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'my-app';
+  title = "multiSelect";
+  public show:boolean = false;
+  public buttonName:any = 'Show';
+
+  ngOnInit () {  }
+
+  
+
+  toggle() {
+    this.show = !this.show;
+
+    // Change the name of the button.
+    if(this.show)  
+      this.buttonName = "Hide";
+    else
+      this.buttonName = "Show";
+  }
+
+  radio=false;
+radio1=false;
+  print(){
+  console.log(this.radio,this.radio1)
+  this.radio = false;
+  this.radio1 = false;
+}
+
+cars = [
+  { id: 1, name: "Air Quality" },
+  { id: 2, name: "Audio"},
+  { id: 3, name: "Displays" },
+  { id: 4, name: "Lighting" },
+  { id: 5, name: "Temperature" },
+];
+
+selected = [{ id: 1, name: "Air Quality" }];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { LOCALE_ID } from "@angular/core";
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+// import { MatButtonModule } from '@angular/material/button';
+
 
 import { AppRoutingModule } from './app-routing.module';
+import { NgSelectModule } from "@ng-select/ng-select";
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+
+// import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+// import { BrowserModule } from "@angular/platform-browser";
+
+// import {
+//   MatFormFieldModule,
+//   MatIconModule,
+//   MatInputModule,
+//   MatListModule,
+//   MatSelectModule,
+//   MatSidenavModule,
+//   MatCardModule,
+//   MatTableModule
+// } from "@angular/material";
+
 
 @NgModule({
   declarations: [
@@ -10,7 +33,23 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    // MatButtonModule,
+    // MatFormFieldModule,
+    // MatIconModule,
+    // MatListModule,
+    // MatInputModule,
+    // MatSelectModule,
+    // MatSidenavModule,
+    // MatCardModule,
+    // MatTableModule,
+    RouterModule, 
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule, 
+    NgSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
